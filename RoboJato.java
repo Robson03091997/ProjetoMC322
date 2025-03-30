fdae70d632ad0342e77b3be210e05eb721e01da6
Robson03091997 <robson.arraespinheiro@hotmail.com>
2025-03-30 18:34:41 -0300

    AtualizacaoRoboJato

--- RoboJato.java
+++ RoboJato.java
@@ -2,19 +2,26 @@
 class RoboJato extends RoboAereo {
     private int velocidade;
 
+    // Declarando o construtor
     protected RoboJato(String nome, int posicaoX, int posicaoY, String direcao, int altitudeMaxima, int velocidade) {
         super(nome, posicaoX, posicaoY, direcao, altitudeMaxima);
         this.velocidade = velocidade;
     }
 
-    //Declarando método get para acelerar, ou reduzir a velocidade.
+    // Método para acelerar
     protected void acelerar(int incremento) {
         this.velocidade += incremento;
         System.out.println(this.nome + " acelerou para " + this.velocidade + " km/h.");
     }
 
+    // Método para reduzir a velocidade
     protected void reduzirVelocidade(int decremento) {
         this.velocidade = Math.max(0, this.velocidade - decremento);
         System.out.println(this.nome + " reduziu a velocidade para " + this.velocidade + " km/h.");
+    }
+
+    // Método getter para a velocidade
+    public int getVelocidade() {
+        return this.velocidade;
     }
 }
