3910a91d84e2c0bbcb5237f789c542dc0705b02d
gabrielthomasdf <gabriel_thomas.df@hotmail.com>
2025-04-24 15:07:40 -0300

    Adicionando relacionamento entre robo e sensores

--- Robo.java
+++ Robo.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 public class Robo {
     protected String nome;
     protected int posicaoX;
     protected int posicaoY;
     protected String direcao;
+    protected int limiteNumSensores;
     public static int numeroDeRobos = 0;
+    public static ArrayList<Sensor> sensores = new ArrayList<>();
 
     //declarando construtores ------------------------------------------------------
     protected Robo(String nome, int posicaoX, int posicaoY, String direcao){
@@ -11,6 +15,15 @@ public class Robo {
         this.posicaoX = posicaoX;
         this.posicaoY = posicaoY;
         this.direcao = direcao;
+        Robo.numeroDeRobos++;
+    }
+
+    protected Robo(String nome, int posicaoX, int posicaoY, String direcao, int limiteNumSensores){
+        this.nome = nome;
+        this.posicaoX = posicaoX;
+        this.posicaoY = posicaoY;
+        this.direcao = direcao;
+        this.limiteNumSensores = limiteNumSensores;
         Robo.numeroDeRobos++;
     }
 
@@ -48,6 +61,23 @@ public class Robo {
         this.direcao = direcao;
     }
 
+    protected void setLimiteNumSensores(int limite){
+        this.limiteNumSensores = limite;
+    }
+
+    protected int getLimiteNumSensores(){
+        return this.limiteNumSensores;
+    }
+
+    protected boolean getSensoresCheio(){
+        if (this.limiteNumSensores == sensores.size()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
 
     //Declarando métodos para mover o robo e para exibir a posição atual -------------------------------------------------------------
     protected void mover (int deltaX, int deltaY){
@@ -64,4 +94,20 @@ public class Robo {
         Ambiente.hangar.forEach(robo -> System.out.println("Robo: " + robo.getNome() + " posição (" + robo.getPosX() + ", " + robo.getPosY()+")"));
     }
     
+    protected void adicionarSensor(Sensor sensor){
+        if (sensores.size() < limiteNumSensores){
+            sensores.add(sensor);
+        }
+    }
+
+    protected void removerSensor(Sensor sensor){
+        sensores.remove(sensor);
+    }
+
+    protected void transfereSensor(Sensor sensor, Robo robo){
+        if (!robo.getSensoresCheio()) {
+            this.removerSensor(sensor);
+            robo.adicionarSensor(sensor);
+        }
+    }
 }
