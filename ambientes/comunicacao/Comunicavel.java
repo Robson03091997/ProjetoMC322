bf85b82963032e7df37fc01aa13df352c2839bec
Robson03091997 <robson.arraespinheiro@hotmail.com>
2025-06-01 19:03:08 -0300

    Adicionando declarações de exceções na interface Comunicavel

--- ambientes/comunicacao/Comunicavel.java
+++ ambientes/comunicacao/Comunicavel.java
@@ -1,16 +1,24 @@
 package ambientes.comunicacao;
 
+import ambientes.robos.RoboDesligadoException;
+
 public interface Comunicavel {
     /**
      * Envia uma mensagem para outro robô comunicável
      * @param destinatario robô que receberá a mensagem
      * @param mensagem conteúdo da mensagem
+     * @throws RoboDesligadoException se o robô estiver desligado
+     * @throws ErroComunicacaoException se houver erro na comunicação
      */
-    void enviarMensagem(Comunicavel destinatario, String mensagem);
+    void enviarMensagem(Comunicavel destinatario, String mensagem) 
+        throws RoboDesligadoException, ErroComunicacaoException;
 
     /**
      * Recebe uma mensagem
      * @param mensagem conteúdo da mensagem recebida
+     * @throws RoboDesligadoException se o robô estiver desligado
+     * @throws ErroComunicacaoException se houver erro na comunicação
      */
-    void receberMensagem(String mensagem);
+    void receberMensagem(String mensagem) 
+        throws RoboDesligadoException, ErroComunicacaoException;
 } 
